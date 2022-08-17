1b5ed1ca10eb08f6978ff247aadcc31919980999
Bruno Soares <brunonobre146@gmail.com>
2022-08-17 20:28:41 -0300

    Dois exercicios condicionais respondidos

---
+++ Pycoder/Exercicio_1.py
@@ -0,0 +1,8 @@
+x = 50
+
+if x > 150:
+    print("Grande")
+elif x > 100 and x <= 150:
+    print("Médio")
+else: 
+    print("Pequeno")
---
+++ Pycoder/Exercicio_2.py
@@ -0,0 +1,10 @@
+x = -2
+
+if x > 150:
+    print("Grande")
+elif x > 100 and x <= 150:
+    print("Médio")
+elif x >= 0 and x <=100:
+    print("Pequeno")
+else:
+    print("Negativo")  
