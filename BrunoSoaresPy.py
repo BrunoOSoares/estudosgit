69b6f5e2642c2066dc8ec18e1e25e2b99ebb531a
Bruno Soares <brunonobre146@gmail.com>
2022-08-17 21:25:19 -0300

    novos ajustes de codigo

--- BrunoSoaresPy.py
+++ BrunoSoaresPy.py
@@ -1,8 +1,7 @@
 nome = input("Qual seu nome? ")
 sobrenome = input("Qual seu sobrenome? ")
+idade = int(input("Digite sua idade: "))
 
 NomeCompleto = nome + ' ' + sobrenome
 
-print(NomeCompleto)
-
-print(nome)
+print(NomeCompleto + " e sua idade é " + str(idade))
---
+++ Funcao.py
@@ -0,0 +1,11 @@
+'''def adicione_15 (m):
+    if m>=100:
+        m=m+15
+        return m
+    else:
+        return ("guarda mais!")'''
+
+def aprender():
+    print("Aprendendo sobre função")
+    return 0
+aprender()
